45f649ac88dcbbb9494bc3dfd8a46b8fffcf3cff
leam <leam.murphy1@gmail.com>
2021-06-24 19:40:10 +0000

    edited queries to be parameterized to protect from SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,42 +9,49 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query is a function that accepts an SQL quety as a JS string.
+// // pool.query is a function that accepts an SQL quety as a JS string.
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students 
-Limit 5;
-`)
-.then(res => {
-  // this produces the same data bc it is the same SQL query. However there is one
-  // important difference, the results come back as an array of JavaScript objects.
-  // This means once the .then gets executed, we are not dealing with the database, 
-  // but rather plan old JS objects.
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students 
+// Limit 5;
+// `)
+// .then(res => {
+//   // this produces the same data bc it is the same SQL query. However there is one
+//   // important difference, the results come back as an array of JavaScript objects.
+//   // This means once the .then gets executed, we are not dealing with the database, 
+//   // but rather plan old JS objects.
+//   console.log(res.rows);
+// })
+// .catch(err => console.error('query error', err.stack));
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
 
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
